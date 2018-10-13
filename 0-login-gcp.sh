ae08d116cae7338344fa17405dc92ebdd0c35ac2
Masatoshi Itoh <masatoshi9953@gmail.com>
2018-10-14 01:01:11 +0900

    add x attrib to sh scripts

--- 0-login-gcp.sh
+++ 0-login-gcp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-gcloud auth login
+gcloud auth configure-docker
+gcloud auth application-default login
+# gcloud auth login
 export PROJECT_ID=`gcloud config get-value project`
 
--- 1-build-container-image.sh
+++ 1-build-container-image.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+gcloud auth configure-docker
 docker build -t gcr.io/${PROJECT_ID}/node-hello-app:v2 .
